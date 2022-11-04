3a69f141c3d78f4b16ad7601b76bcd91c3109c1d
Fabricio Bischoff <112656502+fabriciobischoff@users.noreply.github.com>
2022-11-04 17:25:18 -0300

    Update WebSecurityConfigV2.java

--- linguagens-api/src/main/java/br/com/alura/linguagensapi/configs/security/WebSecurityConfigV2.java
+++ linguagens-api/src/main/java/br/com/alura/linguagensapi/configs/security/WebSecurityConfigV2.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-// Nova recomendação
 @EnableGlobalMethodSecurity(prePostEnabled = true) // necessario para implementar as permissoes dentro do controller
 @Configuration
 public class WebSecurityConfigV2 {
